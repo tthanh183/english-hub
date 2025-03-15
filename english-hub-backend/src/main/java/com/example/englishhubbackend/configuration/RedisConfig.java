43efbdc24da2ec92c58a44dc0e3d5b0fbd95af83
tthanh183 <trantanthanh29@gmail.com>
2025-03-15 22:04:57 +0700

    feat: add redis configuration

---
+++ english-hub-backend/src/main/java/com/example/englishhubbackend/configuration/RedisConfig.java
@@ -0,0 +1,32 @@
+package com.example.englishhubbackend.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+    @Value("${spring.data.redis.host}")
+    private String host;
+    @Value("${spring.data.redis.port}")
+    private int redisPort;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(host, redisPort);
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        return template;
+    }
+}
---
+++ english-hub-backend/src/main/java/com/example/englishhubbackend/configuration/SecurityConfig.java
@@ -0,0 +1,6 @@
+package com.example.englishhubbackend.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+public class SecurityConfig {
+}
