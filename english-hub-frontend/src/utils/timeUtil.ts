29128935eb20c3c5c81434bb352219debfbc8804
tthanh183 <trantanthanh29@gmail.com>
2025-04-08 15:17:16 +0700

    fix: correct longToString function to display correct duration

--- english-hub-frontend/src/utils/timeUtil.ts
+++ english-hub-frontend/src/utils/timeUtil.ts
@@ -1,13 +1,22 @@
 export function longToString(long: number): string {
-  const hours = Math.floor(long / (1000 * 60 * 60)); // Tính số giờ
-  const minutes = Math.floor((long % (1000 * 60 * 60)) / (1000 * 60)); // Tính số phút
-  const seconds = Math.floor((long % (1000 * 60)) / 1000); // Tính số giây
+  const hours = Math.floor(long / (1000 * 60 * 60));
+  const minutes = Math.floor((long % (1000 * 60 * 60)) / (1000 * 60)); 
+  const seconds = Math.floor((long % (1000 * 60)) / 1000); 
 
-  return seconds > 0
-    ? `${seconds} seconds`
-    : minutes > 0
-    ? `${minutes} minutes`
-    : hours > 0
-    ? `${hours} hours`
-    : '0 seconds';
+  let result = '';
+  if (hours > 0) {
+    result += `${hours} hour${hours > 1 ? 's' : ''} `; 
+  }
+  if (minutes > 0) {
+    result += `${minutes} minute${minutes > 1 ? 's' : ''} `; 
+  }
+
+  if (seconds > 0) {
+    result += `${seconds} second${seconds > 1 ? 's' : ''}`; 
+  }
+
+  if (result === '') {
+    result = '0 seconds'; 
+  }
+  return result.trim(); 
 }
