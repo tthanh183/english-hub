0474ddcc03c8079354474025086b2d7b305bccaa
tthanh183 <trantanthanh29@gmail.com>
2025-06-08 15:49:35 +0700

    fix: remove chatpanel from exam page

--- english-hub-frontend/src/layouts/HomeLayout.tsx
+++ english-hub-frontend/src/layouts/HomeLayout.tsx
@@ -1,9 +1,12 @@
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import { ChatButton } from '@/components/home/ChatbotButton';
 
 export default function HomeLayout() {
+  const location = useLocation();
+  const isExamPage = location.pathname.startsWith('/exams');
+
   return (
     <div lang="en" className="flex flex-col min-h-screen bg-gray-50">
       <Header />
@@ -11,7 +14,7 @@ export default function HomeLayout() {
         <Outlet />
       </main>
       <Footer />
-      <ChatButton />
+      {!isExamPage && <ChatButton />}
     </div>
   );
 }
